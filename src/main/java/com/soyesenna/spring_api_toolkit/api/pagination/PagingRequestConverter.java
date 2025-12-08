62efa757bc22b48283b4305d9e92aac3a2f54f38
senna <kjy915875@gmail.com>
2025-12-08 17:47:29 +0900

    feat: paging request jackson converter 구현

---
+++ src/main/java/com/soyesenna/spring_api_toolkit/api/pagination/PagingRequestConverter.java
@@ -0,0 +1,24 @@
+package com.soyesenna.spring_api_toolkit.api.pagination;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagingRequestConverter implements Converter<String, PagingRequest> {
+
+  private final ObjectMapper objectMapper;
+
+  public PagingRequestConverter(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
+  @Override
+  public PagingRequest convert(String source) {
+    try {
+      return objectMapper.readValue(source, PagingRequest.class);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("JSON parsing error", e);
+    }
+  }
+}
